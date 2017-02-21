b6039caea68b457036373bcb6fd4dfce06e3ad9d
Comfort Olorunlero <wio835@my.utsa.com>
2017-02-20 19:17:19 -0600

    Numeric Class Added

---
+++ src/havabol/Numeric.java
@@ -0,0 +1,17 @@
+package havabol;
+
+public class Numeric
+{
+
+	int integerValue;
+	double doubleValue;
+	String strValue;		//display value
+	int type; 				//Integer, Float
+	
+	public Numeric()
+	{
+		//we'll work on this in the future
+		//But here, we have a numeric class!
+	}
+
+}
