fc1a754f66cb8777135f9075d4d7420619484812
Andrew Sanetra <asanetras@gmail.com>
2017-03-20 14:32:56 -0500

    Add basic support for all Execute methods

--- src/havabol/runtime/Execute.java
+++ src/havabol/runtime/Execute.java
@@ -23,7 +23,7 @@ public class Execute {
 		if (resultType == DataType.INTEGER) {
 			
 			if (op2.dataType != DataType.INTEGER) {
-				// TODO: coerce op2 to INTEGER type
+				op2 = op2.asInteger(parser);
 			}
 			
 			res.dataType = DataType.INTEGER;
@@ -33,7 +33,7 @@ public class Execute {
 		} else if (resultType == DataType.FLOAT) {
 			
 			if (op2.dataType != DataType.FLOAT) {
-				// TODO: coerce op2 to FLOAT type
+				op2 = op2.asFloat(parser);
 			}
 			
 			res.dataType = DataType.FLOAT;
@@ -64,7 +64,7 @@ public class Execute {
 		if (resultType == DataType.INTEGER) {
 			
 			if (op2.dataType != DataType.INTEGER) {
-				// TODO: coerce op2 to INTEGER type
+				op2 = op2.asInteger(parser);
 			}
 			
 			res.dataType = DataType.INTEGER;
@@ -74,7 +74,7 @@ public class Execute {
 		} else if (resultType == DataType.FLOAT) {
 			
 			if (op2.dataType != DataType.FLOAT) {
-				// TODO: coerce op2 to FLOAT type
+				op2 = op2.asFloat(parser);
 			}
 			
 			res.dataType = DataType.FLOAT;
@@ -105,7 +105,7 @@ public class Execute {
 		if (resultType == DataType.INTEGER) {
 			
 			if (op2.dataType != DataType.INTEGER) {
-				// TODO: coerce op2 to INTEGER type
+				op2 = op2.asInteger(parser);
 			}
 			
 			res.dataType = DataType.INTEGER;
@@ -115,7 +115,7 @@ public class Execute {
 		} else if (resultType == DataType.FLOAT) {
 			
 			if (op2.dataType != DataType.FLOAT) {
-				// TODO: coerce op2 to FLOAT type
+				op2 = op2.asFloat(parser);
 			}
 			
 			res.dataType = DataType.FLOAT;
@@ -140,7 +140,7 @@ public class Execute {
 		if (resultType == DataType.INTEGER) {
 			
 			if (op2.dataType != DataType.INTEGER) {
-				// TODO: coerce op2 to INTEGER type
+				op2 = op2.asInteger(parser);
 			}
 			
 			res.dataType = DataType.INTEGER;
@@ -150,7 +150,7 @@ public class Execute {
 		} else if (resultType == DataType.FLOAT) {
 			
 			if (op2.dataType != DataType.FLOAT) {
-				// TODO: coerce op2 to FLOAT type
+				op2 = op2.asFloat(parser);
 			}
 			
 			res.dataType = DataType.FLOAT;
@@ -167,68 +167,245 @@ public class Execute {
 
 	public static ResultValue unaryMinus(Parser parser, ResultValue op1)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResultValue res = new ResultValue();
+		res.dataType = op1.dataType;
+		res.structure = Structure.PRIMITIVE;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			res.intValue = op1.intValue * -1;
+		} else if (op1.dataType == DataType.FLOAT) {
+			res.floatValue = op1.floatValue * -1;
+		} else {
+			throw new UnsupportedOperationError("Unary minus may not be used on non-numeric types.");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue unaryNot(Parser parser, ResultValue op1)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResultValue res = op1.asBoolean(parser);
+		res.booleanValue = !res.booleanValue;
+		
+		return res;
 	}
 
 	public static ResultValue less(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue < op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue < op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = op1.strValue.compareTo(op2.strValue) < 0;
+			
+		} else {
+			throw new UnsupportedOperationError("");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue greater(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue > op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue > op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = op1.strValue.compareTo(op2.strValue) > 0;
+			
+		} else {
+			throw new UnsupportedOperationError("");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue lessEqual(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue <= op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue <= op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = op1.strValue.compareTo(op2.strValue) <= 0;
+			
+		} else {
+			throw new UnsupportedOperationError("");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue greaterEqual(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue >= op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue >= op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = op1.strValue.compareTo(op2.strValue) >= 0;
+			
+		} else {
+			throw new UnsupportedOperationError("");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue doubleEqual(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue == op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue == op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = op1.strValue.equals(op2.strValue);
+			
+		} else if (op1.dataType == DataType.BOOLEAN) {
+			
+			op2 = op2.asBoolean(parser);
+			res.booleanValue = op1.booleanValue == op2.booleanValue;
+			
+		} else {
+			throw new UnsupportedOperationError("Unsupported type");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue notEqual(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		if (op1.dataType == DataType.INTEGER) {
+			
+			op2 = op2.asInteger(parser);
+			res.booleanValue = op1.intValue != op2.intValue;
+			
+		} else if (op1.dataType == DataType.FLOAT) {
+			
+			op2 = op2.asFloat(parser);
+			res.booleanValue = op1.floatValue != op2.floatValue;
+			
+		} else if (op1.dataType == DataType.STRING) {
+			
+			op2 = op2.asString(parser);
+			res.booleanValue = !op1.strValue.equals(op2.strValue);
+			
+		} else {
+			throw new UnsupportedOperationError("");
+		}
+		
+		return res;
 	}
 
 	public static ResultValue logicalAnd(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		op1 = op1.asBoolean(parser);
+		op2 = op2.asBoolean(parser);
+		
+		res.booleanValue = op1.booleanValue && op2.booleanValue;
+		
+		return res;
 	}
 
 	public static ResultValue logicalOr(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.BOOLEAN;
+		
+		op1 = op1.asBoolean(parser);
+		op2 = op2.asBoolean(parser);
+		
+		res.booleanValue = op1.booleanValue || op2.booleanValue;
+		
+		return res;
 	}
 
 	public static ResultValue concatenate(Parser parser, ResultValue op1, ResultValue op2)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultValue res = new ResultValue();
+		res.structure = Structure.PRIMITIVE;
+		res.dataType = DataType.STRING;
+		
+		op1 = op1.asString(parser);
+		op2 = op2.asString(parser);
+		
+		res.strValue = op1.strValue.concat(op2.strValue);
+		
+		return res;
 	}
 
 }
