cccdfdbb69aa126991b494e5991de002f5d46e29
Andrew Sanetra <asanetras@gmail.com>
2017-04-12 21:41:37 -0500

    Merge

--- src/havabol/parser/Parser.java
+++ src/havabol/parser/Parser.java
@@ -1110,11 +1110,7 @@ public class Parser {
 			break;
 		case "MAXELEM":
 			argVar = scanner.currentToken.tokenStr;
-<<<<<<< HEAD
-			retVal = Functions.maxElem(this, (STIdentifier) symbolTable.getSymbol(argVar));	
-=======
 			retVal = Functions.maxElem(this, (STIdentifier) symbolTable.getSymbol(argVar));
->>>>>>> refs/remotes/origin/master
 			scanner.getNext();
 			break;
 		case "LENGTH":
