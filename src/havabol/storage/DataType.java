73bad5185eec79a48394e7ebb15d2ed7eae5045a
Andrew Sanetra <asanetras@gmail.com>
2017-04-27 01:15:32 -0500

    Begin function definiton parsing

--- src/havabol/storage/DataType.java
+++ src/havabol/storage/DataType.java
@@ -16,15 +16,23 @@ public enum DataType {
 	public static DataType stringToType(String type) {
 		switch (type.toLowerCase()) {
 		case "int":
+		case "Int":
 			return INTEGER;
 		case "float":
+		case "Float":
 			return FLOAT;
 		case "string":
+		case "String":
 			return STRING;
 		case "bool":
+		case "Bool":
 			return BOOLEAN;
 		case "date":
+		case "Date":
 			return DATE;
+		case "void":
+		case "Void":
+			return VOID;
 		default:
 			return null;
 		}
